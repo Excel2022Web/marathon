04ad8218afbcca6cb3503f4075b3d5ead8f25422
Hemanth R <hemanthrt18@gmail.com>
2022-12-05 12:06:09 +0530

    contact

---
+++ src/data/contactsData.js
@@ -0,0 +1,22 @@
+
+
+export const contactsData = [
+    {
+      id: 1,
+      name: "Nasreen K Basheer",
+      designation: "Chairperson",
+      image: '',
+      phone: "9605204433",
+      email: "nasreenkbasheer.mec@gmail.com",
+      linkedIn: "https://www.linkedin.com/in/nasreen-k-basheer-6795331a5/",
+    },
+    {
+      id: 2,
+      name: "Hemanth Sagar J C",
+      designation: "General Secretary",
+      image: '',
+      phone: "7012910210",
+      email: "hemanthsagar.mec@gmail.com",
+      linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
+    },
+]
