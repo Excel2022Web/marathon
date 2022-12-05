e2e1950d910405696a620364f432f01ff2213ced
Hemanth R <hemanthrt18@gmail.com>
2022-12-05 12:19:26 +0530

    🏃‍♀️🏃‍♂️

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,11 +1,14 @@
-
+import nasreen from "../assets/images/nasreen.jpeg";
+import sebin from "../assets/images/sebin.jpeg";
+import hemanthsagar from "../assets/images/hemanthsagar.jpeg";
+import sania from "../assets/images/sania.png";
 
 export const contactsData = [
     {
       id: 1,
       name: "Nasreen K Basheer",
       designation: "Chairperson",
-      image: '',
+      image: nasreen,
       phone: "9605204433",
       email: "nasreenkbasheer.mec@gmail.com",
       linkedIn: "https://www.linkedin.com/in/nasreen-k-basheer-6795331a5/",
@@ -14,7 +17,25 @@ export const contactsData = [
       id: 2,
       name: "Hemanth Sagar J C",
       designation: "General Secretary",
-      image: '',
+      image: hemanthsagar,
+      phone: "7012910210",
+      email: "hemanthsagar.mec@gmail.com",
+      linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
+    },
+    {
+      id: 3,
+      name: "Sebin Davis",
+      designation: "Joint Secretary",
+      image: sebin,
+      phone: "8921212340",
+      email: "hemanthsagar.mec@gmail.com",
+      linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
+    },
+    {
+      id: 4,
+      name: "Sania Maria Sabu",
+      designation: "General Secretary",
+      image: sania,
       phone: "7012910210",
       email: "hemanthsagar.mec@gmail.com",
       linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
