3811be4686a3d8a239788c1bf8c5b0ac4adc2427
Hemanth R <hemanthrt18@gmail.com>
2022-12-08 22:07:24 +0530

    🏃‍♀️🏃‍♂️

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='banner__component'>
+        banner
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Home, About, Excel, Schedule, Sponsors, Contact } from '../../components'
+import { Home, Banner, About, Excel, Schedule, GMaps, Sponsors, Contact } from '../../components'
 
 import './LandingPage.css'
 
@@ -8,8 +8,10 @@ function LandingPage() {
   return (
     <div className='landingPage'>
       <Home />
+      <Banner />
       <About />
       <Schedule />
+      <GMaps />
       <Excel />
       <Sponsors />
       <Contact />
