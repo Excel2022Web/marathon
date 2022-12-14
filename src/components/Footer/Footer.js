6c3b1f46073995b659092b2ee64614d9b2921e3c
Hemanth R <hemanthrt18@gmail.com>
2022-12-14 11:23:30 +0530

    🏃‍♀️🏃‍♂️

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,69 +11,7 @@ import mec_logo from "../../assets/logo/mec_logo.png"
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="container footer__main">
-        <div className="footer-social">
-          <div className="footer-social-college">
-            <a href="https://www.mec.ac.in/" target="_blank" rel="noreferrer">
-              <img src={mec_logo} className="mec__logo" alt="mec_logo" />
-            </a>
-          </div>
-          
-          <div className="footer-social-socialmedia">
-            <a
-              href="https://www.facebook.com/excelmec"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FiFacebook
-                style={{ border: "none" }}
-                className="footer_social_icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/excelmec/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FiInstagram className="footer_social_icon" />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/excelmec/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FiLinkedin className="footer_social_icon" />
-            </a>
-            <a
-              href="https://twitter.com/excelmec"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FiTwitter className="footer_social_icon" />
-            </a>
-          </div>
 
-          <div className="footer-social-event">
-            <a href="https://excelmec.org/" target="_blank" rel="noreferrer">
-              <img src={excel_logo} className="excel__logo" alt="excel_logo" />
-            </a>
-          </div>
-        </div>
-        {/* <div className="footer_nav">
-            <Link style={{color: 'white'}} to='about'>About Us</Link>            
-            <Link style={{color: 'white'}} to='contact-us'>Contact Us</Link>            
-            <Link style={{color: 'white'}} to='services'>Services</Link>            
-            <Link style={{color: 'white'}} to='privacy-policy'>Privacy Policy</Link>                        
-            <Link style={{color: 'white'}} to='terms'>Terms & Conditions</Link>                                   
-        </div> */}
-        <div className="footer-madewith">
-          Made with{" "}
-          <span>
-            <FaHeart className="footer_heart" />
-          </span>{" "}
-          Excel 2022
-        </div>
-      </div>
     </div>
   )
 }
