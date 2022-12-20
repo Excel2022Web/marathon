4179208f8175a8f900fe628832001a795c060298
Hemanth R <hemanthrt18@gmail.com>
2022-12-20 12:18:31 +0530

    🏃‍♂️🏃‍♀️

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -9,12 +9,17 @@ function Schedule() {
     <div className='schedule' id='schedule'>
       <div className='schedule_container'>
         <h1 className="section__title" data-aos="fade-up" data-aos-duration="1200">Schedule</h1>
-        <div className='schedule_timeline'>
-          <ScheduleCard />
-          <ScheduleCard />
-          <ScheduleCard />
-          <ScheduleCard />
-          <ScheduleCard />
+        {1 === 0 && (
+          <div className='schedule_timeline'>
+            <ScheduleCard />
+            <ScheduleCard />
+            <ScheduleCard />
+            <ScheduleCard />
+            <ScheduleCard />
+          </div>
+        )}
+        <div className='schedule_coming'>
+          <h1>Coming Soon</h1>
         </div>
       </div>
         
