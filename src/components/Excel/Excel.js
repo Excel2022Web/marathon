0a843bede17b427ea6f4b519773c67aa3fb7d34f
Hemanth R <hemanthrt18@gmail.com>
2022-12-20 12:01:12 +0530

    🏃‍♀️🏃‍♂️

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -9,24 +9,24 @@ function Excel() {
   return (
     <div className='excel' id="organizers">
       <div className='excel_container'>
-        <div className='excel_left' data-aos="zoom-in" data-aos-duration="1200">
+        <div className='excel_left'>
             <div className='excel_left_title'>
-              <h4 className='excel_title'>FORTITUDE</h4>
+              <h4 className='excel_title' data-aos="zoom-in" data-aos-duration="1200">FORTITUDE</h4>
               <div className='excel_line'></div>
             </div>
-            <p className='excel_desc'>Fortitude is the mental health club of Model Engineering College. The club aims to spark and engage conversations around mental health in the campus. <br />Established in late 2020, Fortitude has since then conducted several activities to address mental health issues among students and to provide avenues for them to seek professional help.</p>
+            <p className='excel_desc' data-aos="zoom-in" data-aos-duration="1200">Fortitude is the mental health club of Model Engineering College. The club aims to spark and engage conversations around mental health in the campus. <br />Established in late 2020, Fortitude has since then conducted several activities to address mental health issues among students and to provide avenues for them to seek professional help.</p>
           </div>
-          <div className='excel_right' data-aos="zoom-in" data-aos-duration="1200">
+          <div className='excel_right'>
             <div className='excel_right_title'>
-              <h4 className='excel_title'>Excel</h4>
+              <h4 className='excel_title' data-aos="zoom-in" data-aos-duration="1200">Excel</h4>
               <div className='excel_line'></div>
             </div>
-            <p className='excel_desc1'>Excel is the annual national level techno-managerial fest of Govt. Model Engineering College. Conceived in 2001 by the students, Excel has grown exponentially, providing a strong platform which hosts technical, managerial and general events. These events are scheduled over three action packed days. Apart from a series of high profile events like IBeTo (Innovations for a Better Tomorrow), Untitled and .ISSUE!, Excel conducts online competitions, workshops, talks and seminars, video conferences, exhibitions and pro shows.</p>
+            <p className='excel_desc1' data-aos="zoom-in" data-aos-duration="1200">Excel is the annual national level techno-managerial fest of Govt. Model Engineering College. Conceived in 2001 by the students, Excel has grown exponentially, providing a strong platform which hosts technical, managerial and general events. These events are scheduled over three days. Excel conducts online competitions, workshops, talks and seminars, video conferences, exhibitions and pro shows.</p>
           </div>
-          <div className='img_container' data-aos="zoom-in" data-aos-duration="1200">
+          <div className='img_container'>
             <img src={mainImg} alt='' className='excel_img' />
           </div>
-          <div className='img_container1' data-aos="zoom-in" data-aos-duration="1200">
+          <div className='img_container1'>
             <img src={mobileImg} alt='' className='excel_img_mobile' />
           </div>
       </div>
