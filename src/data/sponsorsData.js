01837b2555aa2c19d935e1ebf6a40dd4889d02b4
Hemanth R <hemanthrt18@gmail.com>
2022-12-24 16:48:05 +0530

    🏃‍♀️🏃‍♂️

--- src/data/sponsorsData.js
+++ src/data/sponsorsData.js
@@ -1,5 +1,6 @@
 import ukxpress from  '../assets/logo/ukxpress.jpg'
 import club_fm from  '../assets/logo/club_fm.svg'
+import jubeerich from  '../assets/logo/jubeerich.svg'
 import twentyfournews from  '../assets/logo/twentyfournews.png'
 
 export const sponsorsData = [
@@ -10,15 +11,22 @@ export const sponsorsData = [
         logo: ukxpress
     },
     {
+        id: 4,
+        name: 'Jubeerich',
+        url: '',
+        logo: jubeerich
+    },
+    {
         id: 2,
         name: 'Club FM',
         url: '',
         logo: club_fm
     },
     {
-        id: 2,
+        id: 3,
         name: '24 News',
         url: '',
         logo: twentyfournews
     },
+
 ]
