b260980bc76279e8f236caf01629c510c379aec1
Hemanth R <hemanthrt18@gmail.com>
2023-01-09 14:04:38 +0530

    🏃‍♂️🏃‍♀️

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -13,7 +13,7 @@ function Sponsors() {
           {sponsorsData.map((sponsor) => (
             <a href={sponsor.url} key={sponsor.id} className="sponsor_details">
               <img src={sponsor.logo} alt="" data-aos="fade-up" data-aos-duration="1200"/>
-              {/* <h2>{sponsor.name}</h2> */}
+              <h2 data-aos="fade-up">{sponsor.desc}</h2>
             </a>
           ))}
         </div>
--- src/data/sponsorsData.js
+++ src/data/sponsorsData.js
@@ -1,30 +1,34 @@
-import ukxpress from  '../assets/logo/ukxpress.jpg'
+// import ukxpress from  '../assets/logo/ukxpress.jpg'
 import club_fm from  '../assets/logo/club_fm.svg'
-import jubeerich from  '../assets/logo/jubeerich.svg'
+import leeway from  '../assets/logo/leeway.svg'
 import twentyfournews from  '../assets/logo/twentyfournews.png'
 
 export const sponsorsData = [
-    {
-        id: 1,
-        name: 'UKExpress',
-        url: '',
-        logo: ukxpress
-    },
+    // {
+    //     id: 1,
+    //     name: 'UKExpress',
+    //     desc: '',
+    //     url: '',
+    //     logo: ukxpress
+    // },
     {
         id: 4,
-        name: 'Jubeerich',
+        name: 'Leeway',
+        desc: 'Fitnes Partner',
         url: '',
-        logo: jubeerich
+        logo: leeway
     },
     {
         id: 2,
         name: 'Club FM',
+        desc: 'Radio Partner',
         url: '',
         logo: club_fm
     },
     {
         id: 3,
         name: '24 News',
+        desc: 'Media Partner',
         url: '',
         logo: twentyfournews
     },
