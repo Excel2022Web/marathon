3643bddc6bf3feddd77a8e484e964c0f4948d217
Hemanth R <hemanthrt18@gmail.com>
2023-01-11 13:16:13 +0530

    🏃‍♀️🏃‍♂️

--- src/data/sponsorsData.js
+++ src/data/sponsorsData.js
@@ -1,5 +1,4 @@
 // import ukxpress from  '../assets/logo/ukxpress.jpg'
-import club_fm from  '../assets/logo/club_fm.svg'
 import leeway from  '../assets/logo/leeway.svg'
 import twentyfournews from  '../assets/logo/twentyfournews.png'
 import rotary from  '../assets/logo/rotary.png'
@@ -15,17 +14,17 @@ export const sponsorsData = [
     {
         id: 4,
         name: 'Leeway',
-        desc: 'Fitnes Partner',
+        desc: 'Fitness Partner',
         url: '',
         logo: leeway
     },
-    {
-        id: 2,
-        name: 'Club FM',
-        desc: 'Radio Partner',
-        url: '',
-        logo: club_fm
-    },
+    // {
+    //     id: 2,
+    //     name: 'Club FM',
+    //     desc: 'Radio Partner',
+    //     url: '',
+    //     logo: club_fm
+    // },
     {
         id: 3,
         name: '24 News',
