bab5693d9a91990100ec1f5b6f941292b29aef9d
Hemanth R <hemanthrt18@gmail.com>
2023-02-05 00:26:51 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -50,6 +50,58 @@ function Home() {
               starts in
             </div>
             <div className='timer'>
+import React, { useState, useEffect } from 'react'
+import { HiChevronDoubleRight } from "react-icons/hi";
+
+import './Home.css'
+
+import chart from '../../assets/png/chart.png'
+import landing_avatar from '../../assets/svg/landing_avatar.svg'  
+
+
+const calculateTimeLeft = () => {
+  let difference = +new Date("2023-02-05T10:00:00+05:30") - +new Date(); // MM/DD/YYYY
+  let timeLeft = {};
+
+  if (difference > 0) {
+     timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+     };
+  } else {
+    timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+   };
+  }
+
+  return timeLeft;
+};
+
+function Home() {
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+     const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+     }, 1000);
+
+     return () => clearTimeout(timer);
+  });
+
+  return (
+    <div className='home'>
+      <div className='home__container'>
+          <div className='countdown'>
+            <div className='c_starts'>
+              starts in
+            </div>
+            <div className='timer'>
               <div className='timer_div'>
                 <h1>{timeLeft.days}</h1>
                 <p>days</p>
@@ -67,10 +119,10 @@ function Home() {
         <div className='home__left'>
           <h1>mindful miles <br /><span>matter</span></h1>
           <p>Run with us on 5th February to spread awareness and reduce the stigma around mental illness!</p>
-          <a href="https://rzp.io/l/head-start" target="_blank" rel="noreferrer" className='home_reg_btn'>
-            Register Now
+          <div className='home_reg_btn'>
+            Reg. Closed
             <HiChevronDoubleRight />
-          </a>
+          </div>
         </div>
         <div className='home__right'>
           <img src={landing_avatar} alt="" className='landing_avatar' />
